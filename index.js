60a9b89cf3fae1a70834e3ac037bbbb787993888
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-09-24 17:59:32 +0600

    cart item delete operation

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require('dotenv').config()
+// const { ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cphe2d0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,10 +53,17 @@ async function run() {
 
     // cart item post 
     app.post("/cart",async(req,res)=>{
-     
       const cartItem = req.body;
       const cart = await cartcollection.insertOne(cartItem);
       res.send(cart)
+    })
+
+    //cart item delete api
+    app.delete("/cartdelete/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartcollection.deleteOne(query);
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
